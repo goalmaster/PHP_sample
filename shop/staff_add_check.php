a122d750b0098cdeef402205e61dcb61e26e33fd
m.ryota <ryota.momozaki@gmail.com>
2020-01-25 12:23:15 +0900

    update

--- shop/staff_add_check.php
+++ shop/staff_add_check.php
@@ -8,15 +8,15 @@
 
         <?php
 
-        $staff_name=$_POST['name'];
-        $staff_pass=$_POST['pass'];
-        $staff_pass2=$_POST['pass2'];
+        $staff_name = $_POST['name'];
+        $staff_pass = $_POST['pass'];
+        $staff_pass2 = $_POST['pass2'];
 
-        $staff_name=htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
-        $staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
-        $staff_pass2=htmlspecialchars($staff_pass2,ENT_QUOTES,'UTF-8');
+        $staff_name = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
+        $staff_pass = htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+        $staff_pass2 = htmlspecialchars($staff_pass2,ENT_QUOTES,'UTF-8');
 
-        if($staff_name=='')
+        if($staff_name == '')
         {
             print 'スタッフ名が入力されていません。<br />';
         }
@@ -27,17 +27,17 @@
             print '<br />';
         }
 
-        if($staff_pass=='')
+        if($staff_pass == '')
         {
             print 'パスワードが入力されていません。<br />';
         }
 
-        if($staff_pass!=$staff_pass2)
+        if($staff_pass != $staff_pass2)
         {
             print 'パスワードが一致しません。<br />';
         }
 
-        if($staff_name==''||$staff_pass==''||$staff_pass!=$staff_pass2)
+        if($staff_name == '' || $staff_pass == '' || $staff_pass != $staff_pass2)
         {
             print '<form>';
             print '<input type="button" onclick="history.back()" value="戻る">';
@@ -45,7 +45,7 @@
         }
         else
         {
-            $staff_pass=md5($staff_pass);
+            $staff_pass = md5($staff_pass);
             print '<form method="post" action="staff_add_done.php">';
             print '<input type="hidden" name="name" value="'.$staff_name.'">';
             print '<input type="hidden" name="pass" value="'.$staff_pass.'">';
